03459817edc71acc0c8634c611383b16d746d052
root <root@localhost.localdomain>
2019-02-11 19:44:40 +0000

    added midi file name input when run with no args

--- test.cpp
+++ test.cpp
@@ -1,7 +1,17 @@
+#include<iostream>
 #include"markovgenerator.h"
 
 int main(int argc, char *argv[]) {
 	MarkovGenerator test;
-	test.loadMidi(argv[1]);
+	if (argc > 1) {
+		test.loadMidi(argv[1]);
+	}
+	else {
+		char name[20];
+		std::cout << "enter midi filename: ";
+		std::cin >> name;
+		std::cout << "opening " << name << std::endl;
+		test.loadMidi(name);
+	}
 	return 0;
 }
